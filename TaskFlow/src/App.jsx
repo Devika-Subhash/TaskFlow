309f5632b532d0914127cbde52898c4fb258300d
Devika Subhash <devikasubhash024@gmail.com>
2026-07-29 19:51:00 +0530

    Add edit task functionality

--- TaskFlow/src/App.jsx
+++ TaskFlow/src/App.jsx
@@ -4,17 +4,31 @@ import Navbar from "./components/Navbar";
 function App() {
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
-
+  const [editIndex, setEditIndex] = useState(null);
   function addTask() {
-    if (task.trim() === "") return;
+  if (task.trim() === "") return;
 
+  if (editIndex !== null) {
+    const updatedTasks = [...tasks];
+    updatedTasks[editIndex] = task;
+
+    setTasks(updatedTasks);
+    setEditIndex(null);
+  } else {
     setTasks([...tasks, task]);
-    setTask("");
   }
+
+  setTask("");
+}
 
   function deleteTask(indexToDelete) {
   const updatedTasks = tasks.filter((_, index) => index !== indexToDelete);
   setTasks(updatedTasks);
+}
+
+  function editTask(index) {
+  setTask(tasks[index]);
+  setEditIndex(index);
 }
 
   return (
@@ -30,13 +44,16 @@ function App() {
         onChange={(e) => setTask(e.target.value)}
       />
 
-      <button onClick={addTask}>Add Task</button>
+      <button onClick={addTask}>
+      {editIndex !== null ? "Update Task" : "Add Task"}
+      </button>
 
       <ul>
   {tasks.map((item, index) => (
     <li key={index}>
       {item}
       <button onClick={() => deleteTask(index)}>Delete</button>
+      <button onClick={() => editTask(index)}>Edit </button>
     </li>
   ))}
 </ul>
